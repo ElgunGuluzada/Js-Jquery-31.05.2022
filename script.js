2cc2bbfdcd739f47f319a815c517c857fb93303f
Elgun Guluzada <elgunpg@code.edu.az>
2022-06-01 19:04:36 +0400

    feat : autofill password added

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let si = document.getElementById("si")
 let lo = document.getElementById("logOut")
 let idSi = document.getElementById("idSi")
 let idSu = document.getElementById("idSu")
+let rememberMe = document.getElementById("rememberMe")
+let autofil = document.getElementById("autofil")
+
 let signinContainer = document.getElementById("signin-container")
 let signupContainer = document.getElementById("signup-container")
 
@@ -17,6 +20,16 @@ let signUpRePassword = document.getElementById("signUpRePassword")
 let userLogin = document.getElementById("userLogin")
 let userPassword = document.getElementById("userPassword")
 let loginBtn = document.getElementById("login")
+
+autofil.onclick = function () {
+    this.isChecked = true;
+    if (this.isChecked == true) {
+        let dbArr = JSON.parse(localStorage.getItem("dbUser"));
+        let existUsername = dbArr.find(u => u.Username == userLogin.value);
+        userLogin.value = existUsername.Username
+        userPassword.value = existUsername.Password
+    }
+}
 
 si.onclick = function () {
     this.classList.add("d-none")
@@ -46,8 +59,10 @@ registerBtn.addEventListener("click", function () {
     let dbArr = JSON.parse(localStorage.getItem("dbUser"));
 
     if (signUpEmail.value != "") {
+        console.log(signUpPassword.value);
+        console.log(signUpRePassword.value);
 
-        if (signUpPassword != signUpRePassword) {
+        if (signUpPassword.value != signUpRePassword.value) {
             alert("password is not same")
         }
         else {
@@ -80,7 +95,6 @@ loginBtn.addEventListener("click", function () {
     let existUseremail = dbArr.find(u => u.Email == userLogin.value)
     // console.log(existUsername);
     // console.log(existUseremail);
-
     if (userLogin.value != "") {
         if (existUsername != undefined || existUseremail != undefined) {
             if (userLogin.value == existUsername.Username && userPassword.value == existUsername.Password) {
@@ -96,6 +110,7 @@ loginBtn.addEventListener("click", function () {
         }
     }
     else {
+
         alert("Please Enter Username")
     }
 })
@@ -103,5 +118,7 @@ loginBtn.addEventListener("click", function () {
 lo.onclick = function () {
     this.classList.add("d-none")
     signinContainer.classList.remove("d-none")
-
+    userLogin.value = ""
+    userPassword.value = ""
 }
+
